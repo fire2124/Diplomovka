735f01408beb427b19689d811f176d62d31314d5
fire2124 <filip.reichl@gmail.com>
2020-05-29 14:22:28 +0200

    update traffic

--- Download/trafficSituation.js
+++ Download/trafficSituation.js
@@ -7,75 +7,66 @@ if (!fs.existsSync("./Data/TrafficSituation")) {
 
 function downloadTrafficSituation() {
   var request = require("request");
-
   var options = {
     method: "GET",
     url:
-      "https://portal.minv.sk/wps/portal/domov/sluzby_pz/dopravne_krizove_situacie/!ut/p/a1/04_Sj9CPykssy0xPLMnMz0vMAfGjzOJNDJxdPby8DbzcQ_2MDRwtzQJdQ4ItjCyCDYEKIoEKnN0dPUzMfQwMDEwsjAw8XZw8XMwtfQ0MPM2I02-AAzgaENIfrh-FV4m3KVQBPieCFeBxQ0FuaIRBpqciANyVKis!/dl5/d5/L2dBISEvZ0FBIS9nQSEh/pw/Z7_40CEHJK0JGUN30A96QETS828Q4/res/id=QCPgetSituations/c=cacheLevelPage/=/?regionId=0&categories=ZD6%3BZD7%3BZD8%3BZD1%3BZD2%3BZD3%3BZD5&showPlanned=false",
-    qs: {
-      regionId: "0",
-      categories: "ZD6;ZD7;ZD8;ZD1;ZD2;ZD3;ZD5",
-      showPlanned: "false",
-      fbclid: "IwAR3J1JZWD2eAWA--cwVMkiPzMvrrzJYQkP7F5ExcaT6whpedKi8NkCu0ATE",
+      "https://portal.minv.sk/wps/portal/domov/sluzby_pz/dopravne_krizove_situacie/!ut/p/a1/04_Sj9CPykssy0xPLMnMz0vMAfGjzOJNDJxdPby8DbzcQ_2MDRwtzQJdQ4ItjCyCDYEKIoEKnN0dPUzMfQwMDEwsjAw8XZw8XMwtfQ0MPM2I02-AAzgaENIfrh-FV4m3KVQBPieCFeBxQ0FuaIRBpqciANyVKis!/dl5/d5/L2dBISEvZ0FBIS9nQSEh/pw/Z7_40CEHJK0JGUN30A96QETS828Q4/res/id=QCPgetSituations/c=cacheLevelPage/=/?regionId=0&categories=ZD6%3BZD7%3BZD8%3BZD1%3BZD2%3BZD3%3BZD5&showPlanned=false\n",
+    headers: {
+      Cookie: "DigestTracker=AAABcmANZNU",
     },
-    json: true,
   };
 
   request(options, function (error, response, body) {
     if (error) throw new Error(error);
     const date = new Date();
     const imageDate = Date.parse(date);
-
-    Object.size = function (obj) {
-      var size = 0,
-        key;
-      for (key in obj) {
-        if (obj.hasOwnProperty(key)) size++;
-      }
-      return size;
-    };
-
-    // Get the size of an object
-    var size = Object.size(body.situations);
-    //console.log(size);
-    //let numbers = 0;
+    let bodyFinal = JSON.parse(body);
+    fs.writeFileSync(
+      `./Data/TrafficSituation/${imageDate}_before.json`,
+      JSON.stringify(bodyFinal)
+    );
     let array = [];
-    let globalObject = {};
     let count = 0;
     let time = new Date();
     let currentTime = time.getTime();
-
-    for (let i = 0; i < size; i++) {
+ 
+    bodyFinal.situations.forEach((element) => {
       try {
-        //console.log(i);
-        //console.log(body.situations[i].district.parent.name);
         if (
-          body.situations[i].district.parent.name === "Košický kraj" ||
-          body.situations[i].district.parent.name === "Prešovský kraj"
+          element.district.parent.name === "Košický kraj" ||
+          element.district.parent.name === "Prešovský kraj"
         ) {
-          body.situations[i].Id = ++count;
-          body.situations[i].Type = "Traffic";
-          body.situations[i].CurrentTime = currentTime;
-          delete body.situations[i].photosInfo;
-          delete body.situations[i].id;
-          delete body.situations[i].guid;
-          delete body.situations[i].region;
-          delete body.situations[i].district.parent.parent;
-          delete body.situations[i].district.parent.areaLevel;          
-          //console.log(body.situations[i]);
-            
-          array.push(body.situations[i]);
-         // numbers++;
+          element.Id = ++count;
+          element.Type = "Traffic";
+          element.Current_Time = currentTime;
+          element.district_ID = element.district.id;
+          element.district_Name = element.district.name;
+          element.district_AreaLevel = element.district.areaLevel;
+          element.district_Parent_ID = element.district.parent.id;
+          element.district_Parent_Name = element.district.parent.name;
+          element.district_Parent_Extent = element.district.parent.extent;
+          element.category_Code = element.category.code;
+          element.category_Name = element.category.name;
+          element.status_Code = element.status.code;
+          element.status_Name = element.status.name;
+          delete element.photosInfo;
+          delete element.id;
+          delete element.guid;
+          delete element.region;
+          delete element.district;
+          delete element.category;
+          delete element.status;
+          array.push(element);
         }
       } catch (error) {}
-    }
+    });
     //console.log(array);
-    fs.writeFileSync(
-      `./Data/TrafficSituation/${imageDate}.json`,
-      JSON.stringify(array)
-   );
-    //console.log(numbers);
+
+     fs.writeFileSync(
+       `./Data/TrafficSituation/${imageDate}.json`,
+       JSON.stringify(array)
+     );
   });
 }
 
-setInterval(downloadTrafficSituation, 10000);
+setInterval(downloadTrafficSituation, 15000);
