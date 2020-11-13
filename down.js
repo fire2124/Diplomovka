75e5b626285b2087e235ec995f1a355297255fa4
fire2124 <filip.reichl@gmail.com>
2020-11-13 15:06:10 +0100

    update download data from server
    update download data from server

--- down.js
+++ down.js
@@ -8,14 +8,22 @@ const WeatherKE = require("./Download/weatherKE");
 async function downloadAll() {
     return new Promise(resolve => {
         setInterval(() =>{
-            MhdPO.downloadMhdPO(),
-            SadPO.downloadSadPO(),
-            Trains.downloadTrains(),
-            TrafficSituation.downloadTrafficSituation(),
-            WeatherPO.downloadWeatherPO(),
-            WeatherKE.downloadWeatherKE()
+            MhdPO.downloadMhdPO().then(v=>
+                console.log(v)),
+            SadPO.downloadSadPO().then(v=>
+                console.log(v)),
+            Trains.downloadTrains().then(v=>
+                console.log(v)),
+            TrafficSituation.downloadTrafficSituation().then(v=>
+                console.log(v)),
+            WeatherPO.downloadWeatherPO().then(v=>
+                console.log(v)),
+            WeatherKE.downloadWeatherKE().then(v=>
+                console.log(v))
         },15000
-    )})
+    )
+    resolve()
+})
 }
 downloadAll()
 // setInterval(downloadAll(), 15000);
