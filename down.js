e5ee5b11d53734addd1d185f66134c797a7f2928
fire2124 <filip.reichl@gmail.com>
2020-11-16 11:21:56 +0100

    Update down.js

--- down.js
+++ down.js
@@ -5,25 +5,18 @@ const TrafficSituation = require("./Download/trafficSituation");
 const WeatherPO = require("./Download/weatherPO");
 const WeatherKE = require("./Download/weatherKE");
 
-async function downloadAll() {
-    return new Promise(resolve => {
-        setInterval(() =>{
-            MhdPO.downloadMhdPO().then(v=>
-                console.log(v)),
-            SadPO.downloadSadPO().then(v=>
-                console.log(v)),
-            Trains.downloadTrains().then(v=>
-                console.log(v)),
-            TrafficSituation.downloadTrafficSituation().then(v=>
-                console.log(v)),
-            WeatherPO.downloadWeatherPO().then(v=>
-                console.log(v)),
-            WeatherKE.downloadWeatherKE().then(v=>
-                console.log(v))
-        },15000
-    )
-    resolve()
-})
+const downloadAll = async () => {
+    const promises = [
+        MhdPO.downloadMhdPO,
+        SadPO.downloadSadPO,
+        Trains.downloadTrains,
+        TrafficSituation.downloadTrafficSituation,
+        WeatherPO.downloadWeatherPO,
+        WeatherKE.downloadWeatherKE,
+    ]
+const values = await Promise.all(promises.map(p => p()))
+console.log(values)
+return values
 }
-downloadAll()
-// setInterval(downloadAll(), 15000);
+
+setInterval(downloadAll(), 15000);
